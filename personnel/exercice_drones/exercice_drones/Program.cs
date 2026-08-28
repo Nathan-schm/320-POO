450b6696124904aa294b6531a5b5ce9e9306e6e6
ɴᴀᴛʜᴀɴ <px61pje@eduvaud.ch>
2026-08-28 12:08:12 +0200

    features(drone) : ajout de fonction

--- personnel/exercice_drones/exercice_drones/Program.cs
+++ personnel/exercice_drones/exercice_drones/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -9,24 +10,18 @@ namespace exercice_drones
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
-            Console.Clear();
-
-            string drone = "X-O-X";
             string deadDrone = "-----";
+            int batt = 50;
             int posx = 0;
             int posy = 10;
-            int batt = 50;
-
-            Console.SetCursorPosition(posx, posy);
-            Console.Write(drone);
 
             while (batt > 0)
             {
                 Console.Clear();
-                Console.SetCursorPosition(posx, posy);
-                Console.Write(drone);
+                Drone(posx, posy);
                 posx  ++;
                 batt--;
                 Thread.Sleep(100);
@@ -35,12 +30,19 @@ namespace exercice_drones
             if (batt == 0)
             {
                 Console.Clear();
-                Console.SetCursorPosition(posx, posy );
+                Console.SetCursorPosition(posx, posy);
                 Console.Write(deadDrone);
             }
 
-
             Console.Read();
         }
+        
+        static void Drone(int posx,int posy)
+        {
+            string drone = "X-O-X";
+            Console.SetCursorPosition(posx, posy);
+            Console.Write(drone);
+        }
+        
     }
 }
