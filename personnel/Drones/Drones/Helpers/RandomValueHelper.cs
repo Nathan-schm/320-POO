ea1eda033175afda5d13d2cea7144fe58c5564c5
ɴᴀᴛʜᴀɴ <px61pje@eduvaud.ch>
2026-09-11 11:24:01 +0200

    feature(RandomValueHelper.cs, Config.cs): ajout de 2 classe helpers

---
+++ personnel/Drones/Drones/Helpers/RandomValueHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drones.Helpers
+{
+    internal static class RandomValueHelper
+    {
+        public static Random alea = new Random();
+    }
+}
