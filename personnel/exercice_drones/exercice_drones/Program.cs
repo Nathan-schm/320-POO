05a7bafdafbb7c293746d3a6fc7851a7ecbbf9ea
ɴᴀᴛʜᴀɴ <px61pje@eduvaud.ch>
2026-09-11 09:23:52 +0200

    feature(Programme.cs): Changement d'un tableau a une liste

--- personnel/exercice_drones/exercice_drones/Program.cs
+++ personnel/exercice_drones/exercice_drones/Program.cs
@@ -13,22 +13,25 @@ namespace exercice_drones
 
         static void Main(string[] args)
         {
+
             Console.Clear();
             Console.CursorVisible = false;
 
-            Drone drone1 = new Drone(2, 5, 50);
-            Drone drone2 = new Drone(2, 10, 80);
+            Drone drone1 = new Drone(5,5,80);
+            Drone drone2 = new Drone(3, 3, 60);
 
-            while (drone1._battery > 0 || drone2._battery > 0)
+            List<Drone> flotte = new List<Drone>();
+            
+            while (drone1._battery > 0)
             {
-                Console.Clear();
-                drone1.Changestate();
-                drone1.Draw();
-
-                drone2.Changestate();
-                drone2.Draw();
-
+                foreach (Drone drone in flotte)
+                {
+                    Console.Clear();
+                    drone.Changestate();
+                    drone.Draw();
+                }
                 Thread.Sleep(100);
+
             }
             Console.Read();
         }
